d39b4aa3b5734c877c34fab71f2bb8a3d796a175
Philip T Dodd <phil.dodd@cinnamonapps.co.uk>
2023-02-08 03:51:20 +0000

    Fixed Infinite loop problem

--- src/main/java/org/example/RomanNumerals.java
+++ src/main/java/org/example/RomanNumerals.java
@@ -9,22 +9,22 @@ public class RomanNumerals {
             if (arabic > 1000) {
                 numerals += "M";
                 arabic -= 1000;
-            } else if (arabic > 500) {
+            } else if (arabic >= 500) {
                 numerals += "D";
                 arabic -= 500;
-            } else if (arabic > 100) {
+            } else if (arabic >= 100) {
                 numerals += "C";
                 arabic -= 100;
-            } else if (arabic > 50) {
+            } else if (arabic >= 50) {
                 numerals += "L";
                 arabic -= 50;
-            } else if (arabic > 10) {
+            } else if (arabic >= 10) {
                 numerals += "X";
                 arabic -= 10;
-            } else if (arabic > 5) {
+            } else if (arabic >= 5) {
                 numerals += "V";
                 arabic -= 5;
-            } else if (arabic > 1) {
+            } else if (arabic >= 1) {
                 numerals += "I";
                 arabic -= 1;
             }
